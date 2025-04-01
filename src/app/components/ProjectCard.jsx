b5e2517d5428e6c5b43a93ecc31ec3a2f4d56631
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2025-04-01 17:01:06 +0530

    background of the flipped cards changed

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -36,13 +36,10 @@ const ProjectCard = ({ image, title, text }) => {
             Learn More &gt;
           </div>
         </div>
-        <div
-          style={{ backgroundImage: `url(/${image})` }}
-          className="w-full h-full group relative flip-card-back bg-cover bg-center text-white rounded-lg p-4"
-        >
-          <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1] " />
+        <div className="w-full h-full bg-gradient-to-r from-purple-500 via-blue-400 to-red-500 group relative flip-card-back bg-transparent bg-cover bg-center text-white rounded-lg p-4">
+          <div className="absolute inset-0 w-full h-full rounded-md bg-black opacity-50 z-[-1]" />
           <div className="flex flex-col gap-3 py-3 z-[30]">
-            <h1 className="text-white text-2xl font-semibold ">{title}</h1>
+            <h1 className="text-white text-2xl font-semibold">{title}</h1>
             <p className="text-gray-200 text-sm">{text}</p>
           </div>
         </div>
