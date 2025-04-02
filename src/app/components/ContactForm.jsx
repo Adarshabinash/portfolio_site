9c5078862121da229f6a9c73a9e3d40531b885e4
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2025-04-02 12:04:10 +0530

    form colors changes

--- src/app/components/ContactForm.jsx
+++ src/app/components/ContactForm.jsx
@@ -24,7 +24,7 @@ const ContactForm = () => {
         <input
           placeholder="Your message"
           name="message"
-          className="w-full px-3 py-2 text-sm text-gray-300 placeholder-gray-400 bg-white border-0 rounded shadow "
+          className="w-full px-3 py-2 text-sm text-black placeholder-gray-400 bg-gray-200 border-0 rounded shadow "
         />
         <button className="px-6 mb-5 mt-5 py-3 text-sm font-bold text-white uppercase transition-all rounded duration-150 ease-linear bg-blue-500 hover:bg-blue-400 hover:scale-125% ">
           Send a message
