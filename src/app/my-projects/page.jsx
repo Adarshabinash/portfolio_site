e6ec461f07456386a819fedd516c86c274a34151
Adarsh Kumar Mishra <124551477+Adarshabinash@users.noreply.github.com>
2025-04-17 16:23:45 +0530

    Made responsiveness for the projects page

--- src/app/my-projects/page.jsx
+++ src/app/my-projects/page.jsx
@@ -8,12 +8,12 @@ const Page = () => {
   return (
     <div
       style={{ backgroundImage: "url(/Mountains.jpg)" }}
-      className="w-screen h-screen flex flex-col flex-wrap gap-4 items-center justify-center bg-center bg-cover "
+      className="w-full min-h-screen flex flex-col items-center justify-center bg-center bg-cover py-8 px-4"
     >
-      <h1 className="font-semibold text-4xl text-transparent pb-[20px] bg-gradient-to-r from-blue-700 via-blue-300 to-green-400 bg-clip-text ">
+      <h1 className="font-semibold text-2xl md:text-3xl lg:text-4xl text-transparent pb-4 md:pb-6 bg-gradient-to-r from-blue-700 via-blue-300 to-green-400 bg-clip-text text-center">
         Projects
       </h1>
-      <div className="grid grid-cols-2 gap-[60px] max-w-[90%] max-w-[90%] ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 md:gap-8 lg:gap-10 w-full max-w-screen-lg px-2 sm:px-4 place-items-center">
         {Projects.map((project, index) => (
           <ProjectCard
             key={index}
